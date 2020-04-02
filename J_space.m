0ee1b48218d40e4b5746b4b79f1ec58a14cbf31d
kunaln96 <57232147+kunaln96@users.noreply.github.com>
2020-04-01 23:26:51 -0500

    Add files via upload

--- J_space.m
+++ J_space.m
@@ -5,13 +5,16 @@ function [J_space] = J_space(tab_space,config)
 %space frame
 n = size(tab_space,1);     %depends on no. of links the robot has
 J_space = zeros(6, n);
+exp = eye(4);
 for i=1:n
     S_i = tab_space(i,:)';
-    T = eye(4);
-    if i < n
-        for j = 1:(i-1)
+end
+J_space(:,1) = S_i(1,:);
+      for k=2:n
+         exp2 = eye(4);
+         for j=1:(k-1)
             w = tab_space(j,1:3);
-            v = tab_space(j,4:6);
+            v = tab_space(j,4:6); 
             if w == zeros(1,3)                  %if the joint is prismatic
                R = eye(3);
                trans = -config(j)*v';
@@ -21,12 +24,11 @@ for i=1:n
                 trans = (eye(3)*-config(j)+(1-cos(-config(j)))*w_m+(-config(j)...
                     -sin(-config(j)))*w_m^4)*v';
             end
-            exp = [R, trans;
-                zeros(1,3), 1];
-            T = exp*T;  
-        end     
-    else
-         T = eye(4);   
+            exp =   [R, trans;
+                zeros(1,3), 1]
+            exp2 = exp2*exp;
+         end   
+         T = Ad_T(exp2);
+         J_space(:,k) = T*S_i(:,k);     %%here is the fault
+        end  
     end
-    J_space(:,i) = Ad_T(T)*S_i;   
-end
