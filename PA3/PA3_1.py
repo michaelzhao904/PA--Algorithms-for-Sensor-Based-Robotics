93043df0974bea0f573d0684b31bf3f426481351
michaelzhao904 <michaelzhao904@gmail.com>
2020-04-25 19:36:21 -0500

    Added comments

--- PA3/PA3_1.py
+++ PA3/PA3_1.py
@@ -2,10 +2,11 @@
 
 import numpy as np
 import RegisCalib
-# Load data
 
+# specify data source. e.g. "debug-a" or "unknown-j"
 dataSource = "debug-a"
 
+# load data
 calbody_data = np.loadtxt("HW3-PA1\pa1-{}-calbody.txt".format(dataSource), delimiter=',', skiprows=1)
 calreadings_data = np.loadtxt("HW3-PA1\pa1-{}-calreadings.txt".format((dataSource)), delimiter=',', skiprows=1)
 
@@ -19,62 +20,72 @@ pts_a = calbody_data[N_D:(N_A + N_D), :]
 for i in range(N_frame):
 
     pts_D = calreadings_data[(i * N_tol):(i * N_tol + N_D), :]
+    # compute transformation F_D
     F_D = RegisCalib.registration3D(pts_d, pts_D)
 
     pts_A = calreadings_data[(N_D + i * N_tol):(N_D + i * N_tol + N_A), :]
+    # compute transformation F_A
     F_A = RegisCalib.registration3D(pts_a, pts_A)
 
+    # compute C_i^expected
     for j in range(N_C):
         C_expected[(i * N_C + j), :] = np.linalg.inv(F_D).dot(F_A).dot(np.append(calbody_data[N_D + N_A + j, :], 1))[
                                        0:3]
 # print(C_expected)
 
 # problem PA 3.1.4
+# load EM tracking data
 empivot_data = np.loadtxt("HW3-PA1\pa1-{}-empivot.txt".format(dataSource), delimiter=',', skiprows=1)
 N_G, N_frame_pivot = 6, 12
+# compute midpoint and the translated point coordinates
 G_0 = np.mean(empivot_data[0:N_G, :], 0)
 G_0_rep = np.tile(G_0, (N_G*N_frame_pivot, 1))
 g = empivot_data - G_0_rep
 
 A_em = np.zeros((N_frame_pivot*3, 6))
 b_em = np.zeros((N_frame_pivot*3, 1))
+# compute transformation F_G and formulate least square problem
 for i in range(N_frame_pivot):
-    # pts_G = empivot_data[i*N_G:(i+1)*N_G, :]
     pts_g = g[i*N_G:(i+1)*N_G, :]
     pts_G = g[0 * N_G:(0 + 1) * N_G, :]
     F_G = RegisCalib.registration3D(pts_g, pts_G)
 
     A_em[3*i:(3*i+3), :] = np.append(F_G[0:3, 0:3], -np.eye(3), axis=1)
     b_em[3*i:(3*i+3), 0] = -F_G[0:3, 3]
+
 x_em = np.linalg.lstsq(A_em, b_em, rcond=None)[0]
+# estimated post position with EM probe pivot calibration
 P_xyz_em = x_em[3:]+G_0[:, None]
 
 # problem PA 3.1.5
+# load optical tracking data
 optpivot_data = np.loadtxt("HW3-PA1\pa1-{}-optpivot.txt".format(dataSource), delimiter=',', skiprows=1)
 N_H = 6
-G_0_opt = np.mean(optpivot_data[N_D: (N_D+N_H)], 0)
 
 A_opt = np.zeros((N_frame_pivot*3, 6))
 b_opt = np.zeros((N_frame_pivot*3, 1))
 
 for i in range(N_frame_pivot):
+    # compute transformation F_D and transform the observation to EM tracker coordinates
     pts_D_opt = optpivot_data[i*(N_H+N_D): (i*(N_H+N_D) + N_D), :]
     F_D_opt = RegisCalib.registration3D(pts_d, pts_D_opt)
     pts_h = np.append(optpivot_data[(i*(N_H+N_D) + N_D):(i+1)*(N_H+N_D), :], np.ones((N_H, 1)), 1)
     pts_h = np.linalg.inv(F_D_opt).dot(pts_h.T).T[:, 0:3]
 
     if i == 0:
+        # find the midpoint of transformed h in the first frame
         G_0_opt = np.mean(pts_h, 0)
         pts_H = pts_h - G_0_opt
-
+    # compute transformation F_H and formulate least square problem
     F_H = RegisCalib.registration3D(pts_h, pts_H)
     A_opt[3*i:(3*i+3), :] = np.append(F_H[0:3, 0:3], -np.eye(3), axis=1)
     b_opt[3*i:(3*i+3), 0] = -F_H[0:3, 3]
 
 x_opt = np.linalg.lstsq(A_opt, b_opt, rcond=None)[0]
+# estimated post position with optical probe pivot calibration
 P_xyz_opt = x_opt[3:] + G_0_opt[:, None]
 
 # Output text file
 headers = "{}, {}, solution output for PA1-{}".format(N_C, N_frame, dataSource)
 output = np.concatenate((P_xyz_em.T, P_xyz_opt.T, C_expected),axis=0)
-np.savetxt('output.txt', output, delimiter=',',header=headers, fmt='%10.5f')
+np.savetxt('PA3-{}-output.txt'.format(dataSource), output, delimiter=',',header=headers, fmt='%10.5f')
--- PA3/RegisCalib.py
+++ PA3/RegisCalib.py
@@ -1,20 +1,24 @@
 # function package for PA3 by Qishen
 import numpy as np
 
-
+# define function for Correspondance-based Method Algorithm
 def registration3D(pts_a, pts_b):
     # ptsA: (n,3) 3D data points
     # ptsB: (n,3) 3D data points
     # return T: (4,4) [R p;0 1] transformation matrix
+    # This is an implementation of Correspondence-based Methods algorithm in page 12-16 in W7-L1
+
+    # Step 1: compute the mean value of data set a and b
     ave_a = np.mean(pts_a, 0)
     ave_b = np.mean(pts_b, 0)
+    # Step 2: compute a and b tilde
     n = np.shape(pts_a)[0]
     ave_rep_a = np.tile(ave_a, (n, 1))
     ave_rep_b = np.tile(ave_b, (n, 1))
     np.shape(ave_rep_a)
     tilde_a = pts_a - ave_rep_a
     tilde_b = pts_b - ave_rep_b
-    # find R using SVD approach
+    # Step 3: find R using SVD approach
     H = np.zeros((3, 3))
     for i in range(n):
         a_i = tilde_a[i, :][:, None]
@@ -23,9 +27,10 @@ def registration3D(pts_a, pts_b):
         H += H_i
     u, s, vt = np.linalg.svd(H)
     R = vt.T.dot(u.T)
-    # find p vector
+    # Step 4: find p vector
     p = ave_b - R.dot(ave_a)
     p = p[:, None]
+    # Step 5： find desired transformation
     T = np.c_[R, p]
     T = np.concatenate((T, np.array([[0, 0, 0, 1]])))
     return T
